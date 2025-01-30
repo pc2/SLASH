c4fcb16e422c1ed6aca3999492637b8005333c8f
Alexandru Ulmamei <Alexandru.Ulmamei@amd.com>
2025-01-30 14:14:21 +0000

    Changed gitignore.

---
+++ tests/04_segmented/build_all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# cbfcc9eea9a3e403e89a5028245ab7bccb96d050
+V80PP_GIT="git@gitenterprise.xilinx.com:aulmamei/v80-vitis-flow.git"
+VPP_COMMIT_ID="cbfcc9eea9a3e403e89a5028245ab7bccb96d050"
+
+HLS_BUILD_DIR_ACCUMULATE=build_offset.xcv80-lsva4737-2MHP-e-S
+HLS_BUILD_DIR_INCREMENT=build_dma.xcv80-lsva4737-2MHP-e-S
+DESIGN_NAME=04_example
+HOME_DIR=$(realpath .)
+BUILD_DIR=$(realpath ./build)
+HLS_DIR=$(realpath ./hls)
+
+mkdir -p build
+cd build
+git clone $V80PP_GIT
+cd v80-vitis-flow
+git checkout refactor
+
+VPP_DIR=$(realpath $HOME_DIR/build/v80-vitis-flow)
+
+echo "Running HLS step"
+pushd ${HLS_DIR}
+    make
+popd
+
+echo "Running HW step"
+pushd ${VPP_DIR}
+    ./scripts/v80++ --design-name $DESIGN_NAME --cfg $HOME_DIR/config.cfg --segmented --kernels $HLS_DIR/$HLS_BUILD_DIR_ACCUMULATE/sol1 $HLS_DIR/$HLS_BUILD_DIR_INCREMENT/sol1
+    cp build/$DESIGN_NAME.vrtbin $BUILD_DIR
+popd
+
+# user app build
+echo "Running user app build step"
+pushd ${HOME_DIR}
+    mkdir -p build && cd build
+    cmake ..
+    make -j9
+#    cp $VPP_DIR/build/system_map.xml .
+popd
