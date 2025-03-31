a10ed3fe81724547f6e7002982bbede963018601
Alexandru Ulmamei <Alexandru.Ulmamei@amd.com>
2025-03-31 12:23:22 +0100

    Added GPL license for driver.

--- submodules/pcie-hotplug-drv/driver/pcie_hotplug_ids.h
+++ submodules/pcie-hotplug-drv/driver/pcie_hotplug_ids.h
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ */
+
 #ifndef PCIE_HOTPLUG_IDS
 #define PCIE_HOTPLUG_IDS
 
@@ -20,8 +34,10 @@
  * an entry with all fields set to 0.
  */
 static const struct pci_device_id pcie_hotplug_ids[] = {
-    { PCI_DEVICE(0x10ee, 0x50b4) }, /**< Xilinx V80 PCIe device */
-    { 0, } /**< End of list */
+    {PCI_DEVICE(0x10ee, 0x50b4)}, /**< Xilinx V80 PCIe device */
+    {
+        0,
+    } /**< End of list */
 };
 
-#endif // PCIE_HOTPLUG_IDS
+#endif  // PCIE_HOTPLUG_IDS
